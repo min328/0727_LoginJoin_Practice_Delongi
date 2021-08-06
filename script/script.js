8b68d6fa250edd5a7c2a88a40890bbdb3a94e219
min328 <kmj20212021@gmail.com>
2021-08-06 15:56:09 +0900

    login undder lnb menu is done.

--- script/script.js
+++ script/script.js
@@ -12,4 +12,28 @@ $(function(){
             "display" : "block"
         });
     });
+
+    //로그인 input 아래 LNB메뉴 mouseover시 css 적용
+    $("nav#mainLNB li.lnbLi").mouseover(function(){
+        var imgNum = $(this).attr("data-img-num");
+        var imgSrc = "images/icon" + imgNum +"_on.png";
+        $(this).children("#imgBackground").css({
+            "background-color" : "rgb(50, 137, 232)"
+        });
+        $(this).children("a").css({
+            "color" : "rgb(50, 137, 232)"
+        });
+        $(this).children("#imgBackground").children("img").attr("src", imgSrc);  
+    })
+    $("nav#mainLNB li.lnbLi").mouseout(function(){
+        var imgNum = $(this).attr("data-img-num");
+        var imgSrc = "images/icon" + imgNum +".png";
+        $(this).children("#imgBackground").css({
+            "background-color" : "#fff"
+        });
+        $(this).children("a").css({
+            "color" : "#555"
+        });
+        $(this).children("#imgBackground").children("img").attr("src", imgSrc);  
+    })
 });
